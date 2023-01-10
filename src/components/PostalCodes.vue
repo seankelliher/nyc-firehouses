2cc5266dfc1bd09ccc959952d686e1d05144367a
Sean Kelliher <sbkelliher@icloud.com>
2023-01-09 23:40:49 -0500

    Re-add 'PostalCodes.vue' component.

---
+++ src/components/PostalCodes.vue
@@ -0,0 +1,29 @@
+<template>
+    <section id="postals">
+        <div
+            class="link"
+            v-for="postal in postals"
+            :key="postal"
+            @click="$emit('chosenPostalCode', postal)"
+        >
+            {{ postal }}
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: "PostalCodes",
+    components: {},
+    props: {
+        postals: {
+            type: Array,
+            required: true
+        }
+    },
+    emits: [
+        "chosenPostalCode"
+    ]
+};
+</script>
