38cc4a50ad3e1126692966dbe0c758d9223ed6a6
Sean Kelliher <sbkelliher@icloud.com>
2023-01-12 14:21:45 -0500

    Add FireHouses.vue component.

---
+++ src/components/FireHouses.vue
@@ -0,0 +1,28 @@
+<template>
+    <section
+        class="fire-house"
+        v-for="house in houses"
+        :key="house.facilityname"
+    >
+        <dl>
+            <dt>{{ house.facilityname }}</dt>
+            <dd>{{ house.facilityaddress }}</dd>
+            <dd>{{ house.borough }}, {{ house.postcode }}</dd>
+            <dd>Latitude: {{ house.latitude }}</dd>
+            <dd>Longitude: {{ house.longitude }}</dd>
+        </dl>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: "FireHouses",
+    props: {
+        houses: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
