742a815427fa775dbbd046d11684c88b0479f95a
Sean Kelliher <sbkelliher@icloud.com>
2023-01-14 16:49:25 -0500

    Modify class name from 'link' to 'nav-link'.

--- src/components/FullNav.vue
+++ src/components/FullNav.vue
@@ -1,10 +1,10 @@
 <template>
     <nav>
-        <div class="link"><router-link to="/bronx">Bronx</router-link></div>
-        <div class="link"><router-link to="/brooklyn">Brooklyn</router-link></div>
-        <div class="link"><router-link to="/manhattan">Manhattan</router-link></div>
-        <div class="link"><router-link to="/queens">Queens</router-link></div>
-        <div class="link"><router-link to="/staten">Staten Island</router-link></div>
+        <div class="nav-link"><router-link to="/bronx">Bronx</router-link></div>
+        <div class="nav-link"><router-link to="/brooklyn">Brooklyn</router-link></div>
+        <div class="nav-link"><router-link to="/manhattan">Manhattan</router-link></div>
+        <div class="nav-link"><router-link to="/queens">Queens</router-link></div>
+        <div class="nav-link"><router-link to="/staten">Staten Island</router-link></div>
     </nav>
 </template>
 
