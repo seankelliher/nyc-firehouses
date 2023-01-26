8c80d8937d42a35ce32bf77bcde56dcdb8023852
Sean Kelliher <sbkelliher@icloud.com>
2023-01-26 17:50:33 -0500

    Add 'map' icon. Add 'not found' view.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <PageTitle title="404 - Not Found" />
+
+    <p>The page you requested does not exist.</p>
+
+</template>
+
+<script>
+import PageTitle from "../components/PageTitle.vue";
+
+export default {
+    name: "NotFound",
+    components: {
+        PageTitle
+    }
+};
+</script>
