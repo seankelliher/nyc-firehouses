924266ee059acc0083aa998f82796c90bf751218
Sean Kelliher <sbkelliher@icloud.com>
2023-04-15 11:51:56 -0400

    Add 'UniFooter' component.

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,23 @@
     <header>
         <h1><a href="/">Firehouses in New York City</a></h1>
     </header>
+
     <FullNav />
-  <!--<div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>-->
-<router-view></router-view>
+
+    <router-view></router-view>
+
+    <UniFooter />
 </template>
 
 <script>
 import FullNav from "./components/FullNav.vue";
+import UniFooter from "./components/UniFooter.vue";
 
 export default {
     name: "App",
     components: {
-        FullNav
+        FullNav,
+        UniFooter
     }
 };
 </script>
