4942614aacfe41b40233d63abfd901f021bf9028
Sean Kelliher <sbkelliher@icloud.com>
2023-04-15 11:51:25 -0400

    Add 'UniFooter' component.

---
+++ src/components/UniFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer>
+        <p class="light-txt">Use 911 for emergencies, 311 for everything else.</p>
+    </footer>
+</template>
+
+<script>
+
+export default {
+    name: "UniFooter",
+    components: {},
+    props: {}
+};
+</script>
