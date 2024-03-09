7001056fef4c00a6fe472ad1ebdf1a5180d94129
Sean Kelliher <sbkelliher@icloud.com>
2024-03-09 12:19:45 -0500

    Add new components. Modify colors in map.

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,58 @@
 <script setup>
 import HeaderPart from "./components/HeaderPart.vue";
+import RibbonPart from "./components/RibbonPart.vue";
+import SectionPart from "./components/SectionPart.vue";
 import HouseMap from "./components/HouseMap.vue";
+import FooterPart from "./components/FooterPart.vue";
 import "../node_modules/mapbox-gl/dist/mapbox-gl.css";
 </script>
 
 <template>
     <div id="container">
-        <HeaderPart headline="Firehouses in New York City" />
+        <HeaderPart headline="Fire Department" />
+        <RibbonPart headline="Firehouses" />
+        <SectionPart />
         <HouseMap />
+        <FooterPart />
     </div>
 </template>
 
 <style>
     /* Map styles here. Mixed results placing in style.css */
     .mapboxgl-popup-anchor-top .mapboxgl-popup-tip {
-        border-bottom-color: #ec1423;
+        border-bottom-color: #004685;
     }
 
     .mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip {
-        border-top-color: #ec1423;
+        border-top-color: #004685;
     }
 
     .mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip {
-        border-bottom-color: #ec1423;
+        border-bottom-color: #004685;
     }
 
     .mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip {
-        border-bottom-color: #ec1423;
+        border-bottom-color: #004685;
     }
 
     .mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip {
-        border-top-color: #ec1423;
+        border-top-color: #004685;
     }
 
     .mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip {
-        border-top-color: #ec1423;
+        border-top-color: #004685;
     }
 
     .mapboxgl-popup-anchor-right .mapboxgl-popup-tip {
-        border-left-color: #ec1423;
+        border-left-color: #004685;
     }
 
     .mapboxgl-popup-anchor-left .mapboxgl-popup-tip {
-        border-right-color: #ec1423;
+        border-right-color: #004685;
     }
 
     .mapboxgl-popup-content {
-            border: 2px solid #0f86bd;
-            border: 2px solid #71787e;
-            background: #f6faff; /* darker */
+            border: 2px solid #656100;
+            background: #fff63f; /* darker */
     }
 </style>
