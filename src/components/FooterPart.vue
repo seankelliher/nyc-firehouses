e1b09795e93bdf0cd773646e0bd422114337b611
Sean Kelliher <sbkelliher@icloud.com>
2024-03-09 12:17:12 -0500

    Renovating. Add components FooterPart.vue, RibbonPart.vue, SectionPart.vue.

---
+++ src/components/FooterPart.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+    <footer>
+        <ul>
+            <li>Laura Kavanagh, Fire Commissioner</li>
+            <li>Joseph Pfeifer, First Deputy Commissioner</li>
+            <li>John J. Hodgens, Chief of Department</li>
+        </ul>
+
+        <figure class="logo-footer">
+            <img src="/nyc-logo.svg" alt="Logo for New York City government">
+        </figure>
+    </footer>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/SectionPart.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <div class="callout">
+            <h3>About</h3>
+            <p>The FDNY began in 1865 and now operates 219 firehouses within the five boroughs of the Bronx, Brooklyn, Manhattan, Queens, and Staten Island. It responds to more than a million emergencies every year.</p>
+        </div>
+        <figure class="logo-callout">
+            <img src="/fdny-logo.svg" alt="Fire Department City of New York logo">
+        </figure>
+    </section>
+</template>
+
+<style scoped>
+</style>
